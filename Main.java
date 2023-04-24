54a498688602633efa7574fcc359245761a24958
nerpompom <88094409+nerpompom@users.noreply.github.com>
2023-04-24 23:51:49 +0700

    Add 4 files
    CountMe, Main, MyArrayDataException, MyArraySizeException

--- Main.java
+++ Main.java
@@ -1,34 +1,40 @@
-package com.homeproject.hm_lesson_1;
+package com.homeproject.hw_lesson_2;
+
+import java.io.FileNotFoundException;
 
 public class Main {
+    public static void main (String[] args) {
 
-    public static void main(String[] args){
-        
-        Team[] participantArray = new Team[4];
-
-        participantArray[0] = new Team ("Star", "Ivan", 20, 0.5);
-        participantArray[1] = new Team("Star", "Irina",18,0.6);
-        participantArray[2] = new Team("Star", "Oleg", 25, 0.9);
-        participantArray[3] = new Team("Star", "Olga", 31, 0.7);
-
-        participantArray[0].teamInfo();
-        participantArray[1].teamInfo();
-        participantArray[2].teamInfo();
-        participantArray[3].teamInfo();
-
-        Course course = new Course(0.4, 0.5, 0.6, 0.7, 0.8);
-
-        System.out.println(course.doIt(participantArray[0]));
-        System.out.println(course.doIt(participantArray[1]));
-        System.out.println(course.doIt(participantArray[2]));
-        System.out.println(course.doIt(participantArray[3]));
-
-        participantArray[0].winInfo(course);
-        participantArray[1].winInfo(course);
-        participantArray[2].winInfo(course);
-        participantArray[3].winInfo(course);
+        String[][] strArray = new String[4][3];
+        strArray[0][0] = "1";
+        strArray[0][1] = "2";
+        strArray[0][2] = "3";
+        strArray[0][3] = "4";
+        strArray[1][0] = "5";
+        strArray[1][1] = "6";
+        strArray[1][2] = "7";
+        strArray[1][3] = "8";
+        strArray[2][0] = "9";
+        strArray[2][1] = "10";
+        strArray[2][2] = "11";
+        strArray[2][3] = "12";
 
 
+        CountMe countMe = new CountMe();
+
+        try {
+            countMe.arraySize(strArray);
+        }
+        catch (MyArraySizeException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            countMe.arraySymbol(strArray);
+        }
+        catch (MyArrayDataException e) {
+            e.printStackTrace();
+        }
 
     }
 }
